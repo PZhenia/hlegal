bf66a23fe07d118a3a588c16307bf1aa569e038c
Zhenia <zheniapravdenko@gmail.com>
2024-11-25 23:37:33 +0200

    update project

---
+++ js/mobile_menu.js
@@ -0,0 +1,35 @@
+// Завантаження header
+fetch('header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('header').innerHTML = data;
+
+        //  події для меню
+        const hamburger = document.querySelector(".hamburger");
+        const navMenu = document.querySelector(".nav-menu");
+
+        // Переконуємося, що елементи існують
+        if (hamburger && navMenu) {
+            hamburger.addEventListener("click", () => {
+                hamburger.classList.toggle("active");
+                navMenu.classList.toggle("active");
+            });
+
+            // Закриття меню при натисканні на лінки
+            document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+                hamburger.classList.remove("active");
+                navMenu.classList.remove("active");
+            }));
+        } else {
+            console.error("Elements .hamburger or .nav-menu not found!");
+        }
+    })
+    .catch(error => console.error("Error loading header:", error));
+
+// Завантаження footer
+fetch('footer.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('footer').innerHTML = data;
+    })
+    .catch(error => console.error("Error loading footer:", error));
